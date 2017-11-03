d6595dd2bdd0404caf67b7ce222369d77bee859b
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-11-03 14:18:32 +0200

    Added Model Genre

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171027212805) do
+ActiveRecord::Schema.define(version: 20171103121230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20171027212805) do
     t.string "author"
     t.text "summary"
     t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
